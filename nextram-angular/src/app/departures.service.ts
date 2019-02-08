3fa8b37d1ec43d9be8f1b5c6cad6b7c73a152b3f
Kenneth <14309096+krusli@users.noreply.github.com>
2019-02-08 21:12:14 +0700

    Fix some security vulns

--- nextram-angular/src/app/departures.service.ts
+++ nextram-angular/src/app/departures.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Headers, Http } from '@angular/http';
 import { DeparturesData } from './departures';
 
-import 'rxjs/add/operator/toPromise';
+
 
 @Injectable()
 export class DeparturesService {
